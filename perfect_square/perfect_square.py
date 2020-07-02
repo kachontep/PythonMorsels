d87a77955dcf4c8a64e464ef6d81443f57adf2c3
Kajornthep Piyanun <kachontep@outlook.co.th>
2020-07-02 16:51:16 +0700

    Perfect square is refactored to separate checked number from class

--- perfect_square/perfect_square.py
+++ perfect_square/perfect_square.py
@@ -5,31 +5,31 @@ from typing import Union
 
 
 def is_perfect_square(n: Union[int, float, Decimal, complex], *, complex: bool = False) -> bool:
-    return PerfectSquare(n, complex).check()
+    return PerfectSquare(complex).check(n)
 
 
 class PerfectSquare:
-    def __init__(self, n, complex):
-        self.n = n
-        self._check = self.check_decimal
-        if complex:
-            self._check = self.check_complex
+    def __init__(self, complex_):
+        if complex_:
+            self._check = self._check_complex_num
+        else:
+            self._check = self._check_decimal_num
 
-    def check(self):
-        return self._check(self.n)
+    def check(self, n):
+        return self._check(n)
 
-    def _check_int(self, n):
+    def _check_int_num(self, n):
         return int(math.ceil(n)) - int(math.floor(n)) == 0
 
-    def check_complex(self, c):
+    def _check_complex_num(self, c):
         sqrt_n = cmath.sqrt(c)
-        real_int = self._check_int(sqrt_n.real)
-        imag_int = self._check_int(sqrt_n.imag)
+        real_int = self._check_int_num(sqrt_n.real)
+        imag_int = self._check_int_num(sqrt_n.imag)
         result = real_int and imag_int
         return result
 
-    def check_decimal(self, n):
-        if not isinstance(self.n, (int, float, Decimal)):
+    def _check_decimal_num(self, n):
+        if not isinstance(n, (int, float, Decimal)):
             raise TypeError(
                 "Unsupported types: int, float, Decimal and complex types are allowed")
         n = Decimal(str(n))
