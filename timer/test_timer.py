30ece11a982b5834c135c5d839ff053de169add4
Kajornthep Piyanun <kachontep@outlook.co.th>
2020-07-20 23:44:19 +0700

    Complete timer problem both exercise and bonuses

--- timer/test_timer.py
+++ timer/test_timer.py
@@ -55,7 +55,7 @@ class TimerTests(unittest.TestCase):
         self.assertLess(elapsed1, timer.elapsed)
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    # @unittest.expectedFailure
     def test_runs_recorded(self):
         timer1 = Timer()
         timer2 = Timer()
@@ -77,7 +77,7 @@ class TimerTests(unittest.TestCase):
         self.assertTimeEqual(timer2.runs[1], 0.002)
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    # @unittest.expectedFailure
     def test_works_as_decorator(self):
         @Timer
         def wait_randomly(*args, **kwargs):
@@ -91,7 +91,7 @@ class TimerTests(unittest.TestCase):
         self.assertEqual(wait_randomly.runs, [wait_randomly.elapsed])
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    # @unittest.expectedFailure
     def test_stat_recorded(self):
         wait = Timer(sleep)
         wait(0.002)
--- timer/timer.py
+++ timer/timer.py
@@ -1,14 +1,24 @@
+from contextlib import contextmanager
 from timeit import default_timer
 
 
 class Timer:
-    def __init__(self):
+    def __init__(self, fn=None):
         self._start = 0
         self._end = 0
+        self._runs = list()
+        self._fn = fn
 
     @property
     def elapsed(self):
-        return self._end - self._start
+        return self._runs[-1] if self._runs else 0
+
+    @property
+    def runs(self):
+        return self._runs.copy()
+
+    def _record_run(self):
+        self._runs.append(self._end - self._start)
 
     def __enter__(self):
         self._start = default_timer()
@@ -16,3 +26,28 @@ class Timer:
 
     def __exit__(self, exc_type=None, exc_value=None, exc_traceback=None):
         self._end = default_timer()
+        self._record_run()
+
+    def __call__(self, *args, **kwargs):
+        with self:
+            return self._fn(*args, **kwargs)
+
+    @property
+    def min(self):
+        return min(self._runs)
+
+    @property
+    def max(self):
+        return max(self._runs)
+
+    @property
+    def mean(self):
+        return sum(self._runs) / len(self._runs)
+
+    @property
+    def median(self):
+        pos, odd = divmod(len(self._runs), 2)
+        if odd:
+            return self._runs[pos]
+        else:
+            return (self._runs[pos-1] + self._runs[pos]) / 2
