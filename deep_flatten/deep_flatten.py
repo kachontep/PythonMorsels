a0a6cbed2900ec769576c7a915fe5abf12738cb1
Kajornthep Piyanun <kachontep@outlook.co.th>
2020-07-21 20:38:06 +0700

    Extract method for check iterable and string type

--- deep_flatten/deep_flatten.py
+++ deep_flatten/deep_flatten.py
@@ -2,9 +2,18 @@ from collections.abc import Iterable
 
 
 def deep_flatten(ll):
-    if not isinstance(ll, Iterable) \
-            or isinstance(ll, (str, bytes)):
+    if not is_iterable(ll):
+        yield ll
+    elif is_string(ll):
         yield ll
     else:
         for e in ll:
             yield from deep_flatten(e)
+
+
+def is_iterable(ll):
+    return isinstance(ll, Iterable)
+
+
+def is_string(ll):
+    return isinstance(ll, (str, bytes))
