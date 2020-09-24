09724d404c197af2dc025924d6accad488fa4d8c
Kajornthep Piyanun <kajornthep@thezouth.com>
2020-09-24 20:02:18 +0700

    Complete meetup_date main problem and first bonus

--- meetup_date/meetup_date.py
+++ meetup_date/meetup_date.py
@@ -2,12 +2,16 @@ import datetime
 import calendar
 
 
-def meetup_date(year, month):
+def meetup_date(year, month, nth=4, weekday=3):
     c = calendar.monthcalendar(year, month)
-    first_week, fourth_week, fifth_week = c[0], c[3], c[4]
-    if first_week[calendar.THURSDAY]:
-        fourth_thursday = fourth_week[calendar.THURSDAY]
+    first_week, fourth_week, fifth_week = (
+        c[0],
+        c[nth - 1],
+        (c[nth] if nth < len(c) else None),
+    )
+    if first_week[weekday]:
+        fourth_thursday = fourth_week[weekday]
     else:
-        fourth_thursday = fifth_week[calendar.THURSDAY]
+        fourth_thursday = fifth_week[weekday]
     return datetime.date(year, month, fourth_thursday)
 
