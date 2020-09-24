208cb8cdc8a1377eca3ac1fe3bcf35ea26d88fa1
Kajornthep Piyanun <kajornthep@thezouth.com>
2020-09-24 20:59:20 +0700

    Add another solution for meetup_date problem

--- meetup_date/meetup_date.py
+++ meetup_date/meetup_date.py
@@ -1,3 +1,4 @@
+from calendar import week
 import datetime
 import calendar
 from enum import IntEnum
@@ -13,7 +14,7 @@ class Weekday(IntEnum):
     SUNDAY = 6
 
 
-def meetup_date(year, month, nth=4, weekday=3):
+def meetup_date_(year, month, nth=4, weekday=3):
     c = calendar.monthcalendar(year, month)
 
     if nth > 0:
@@ -35,4 +36,43 @@ def meetup_date(year, month, nth=4, weekday=3):
         fourth_thursday = next_nth_week[weekday]
 
     return datetime.date(year, month, fourth_thursday)
+
+
+NUM_DAYS_OF_MONTH = [
+    31,
+    28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+]
+
+
+def number_of_days_in_month(year, month):
+    if month == 2 and (year % 4 == 0 and year % 100 == 0 or year % 400 == 0):
+        return NUM_DAYS_OF_MONTH[month - 1] + 1
+    else:
+        return NUM_DAYS_OF_MONTH[month - 1]
+
+
+def meetup_date(year, month, nth=4, weekday=Weekday.THURSDAY):
+    num_of_days = number_of_days_in_month(year, month)
+    if nth > 0:
+        start = datetime.date(year, month, 1)
+        diff = 1
+    else:
+        start = datetime.date(year, month, num_of_days)
+        diff = -1
+    days_in_month = [
+        start + datetime.timedelta(days=diff * i) for i in range(num_of_days)
+    ]
+    recurrings = [d for d in days_in_month if d.weekday() == weekday]
+    result = recurrings[int(abs(nth)) - 1]
+    return result
 
