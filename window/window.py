50a7b0a0a088ac66572b885753d532e7a30e65cb
Kajornthep Piyanun <kajornthep@thezouth.com>
2020-11-05 19:00:31 +0700

    Refactoring window problem

--- window/window.py
+++ window/window.py
@@ -7,22 +7,28 @@ class WindowSlider:
 
     def capture(self, fillvalue=None):
         items = []
-        captured = False
         for e in self._iterable:
             items.append(e)
             self._lastitems = items
             if len(items) == self._size:
-                captured = True
-            if not captured:
-                continue
-            yield tuple(items)
-            self._len += 1
-            items = items[1:]
-        if self._len == 0 and len(self._lastitems) < self._size:
+                yield tuple(items)
+                self._len += 1
+                items = items[1:]
+        if self._had_pendings():
             yield (
                 tuple(self._lastitems)
                 + ((fillvalue,) * (self._size - len(self._lastitems)))
             )
+
+    def __len__(self):
+        return self._len
+
+    @property
+    def lastitems(self):
+        return self._lastitems
+
+    def _had_pendings(self):
+        return self._len == 0 and len(self._lastitems) < self._size
 
 
 def window(iterable, size, **kwargs):
