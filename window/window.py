77d70bf2402607abfa8fe292edecb4d9850c3765
Kajornthep Piyanun <kajornthep@thezouth.com>
2020-11-05 18:50:11 +0700

    Complete window problem and all bonuses

--- window/window.py
+++ window/window.py
@@ -1,16 +1,31 @@
-import itertools
+class WindowSlider:
+    def __init__(self, iterable, size):
+        self._iterable = iterable
+        self._size = size
+        self._len = 0
+        self._lastitems = []
+
+    def capture(self, fillvalue=None):
+        items = []
+        captured = False
+        for e in self._iterable:
+            items.append(e)
+            self._lastitems = items
+            if len(items) == self._size:
+                captured = True
+            if not captured:
+                continue
+            yield tuple(items)
+            self._len += 1
+            items = items[1:]
+        if self._len == 0 and len(self._lastitems) < self._size:
+            yield (
+                tuple(self._lastitems)
+                + ((fillvalue,) * (self._size - len(self._lastitems)))
+            )
 
 
-def window(iterable, size):
-    items = []
-    captured = False
-    for e in iterable:
-        items.append(e)
-        if len(items) == size:
-            captured = True
-        if not captured:
-            continue
-        yield tuple(items)
-        items = items[1:]
-    if not captured:
-        return [tuple(items) + ((None,) * (size - len(items)))]
+def window(iterable, size, **kwargs):
+    fillvalue = kwargs.get("fillvalue", None)
+    slider = WindowSlider(iterable, size)
+    return slider.capture(fillvalue)
