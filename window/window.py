cff94236913d6f1d18a0341fcbdedb6e7c4be533
Kajornthep Piyanun <kajornthep@thezouth.com>
2020-11-05 17:33:23 +0700

    Done with window problem but no bonuses

---
+++ window/window.py
@@ -0,0 +1,11 @@
+def window(iterable, size):
+    items = []
+    captured = False
+    for e in iterable:
+        items.append(e)
+        if len(items) == size:
+            captured = True
+        if not captured:
+            continue
+        yield tuple(items)
+        items = items[1:]
